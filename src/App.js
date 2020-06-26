80f94cd478ece0f4c59bc7b14c34ef0e1329adfd
byohannes <42332389+byohannes@users.noreply.github.com>
2020-06-26 23:43:40 +0100

    Balance component and income expenses component added as well

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react'; 
 import {Header} from "./components/Header";
+import {Balance} from "./components/Balance";
+import {IncomeExpenses} from "./components/IncomeExpenses";
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Header/> 
+      <Header/>
+      <div className="container">
+
+       <Balance/>
+       <IncomeExpenses/>
+
+      </div>
+
     </div>
   );
 }
